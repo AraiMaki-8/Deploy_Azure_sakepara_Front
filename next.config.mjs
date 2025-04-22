741db1820b5bc3fa19ef946ee8f72d98c150cfb8
AraiMaki-8 <maki.mi.4416@gmail.com>
2025-04-22 10:02:47 +0900

    ,env、next.config追加

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,  // ESLintエラーを無視する設定
+  }
+}
+
+module.exports = nextConfig
